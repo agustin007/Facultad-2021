c70deaf61f3ae711d34407eef7718bd0ba8d4fb9
Gonzalo Medrano <gonzalomedrano09@gmail.com>
2021-04-18 15:19:31 -0300

    Hago explicitas algunos datos en tp1

--- SIM/TP1/generador_numeros/dominio/controlador_generador_numeros_pseudoaleatorios.py
+++ SIM/TP1/generador_numeros/dominio/controlador_generador_numeros_pseudoaleatorios.py
@@ -42,10 +42,14 @@ class ControladorGeneradorNumerosPseudoaleatorios:
 
     def obtener_intervalos(self, cantidad_intervalos):
 
+        # Defino mínimo y máximo
+        minimo = 0
+        maximo = 1
+
         # Genero lista de intervalos
         intervalos = []
-        max_intervalo = 0
-        paso = Decimal(1 / cantidad_intervalos).quantize(SIXPLACES)
+        max_intervalo = minimo
+        paso = Decimal((maximo - minimo) / cantidad_intervalos).quantize(SIXPLACES)
         for i in range(0, cantidad_intervalos):
             min_intervalo = Decimal(max_intervalo).quantize(SIXPLACES)
             max_intervalo = Decimal(min_intervalo + paso).quantize(SIXPLACES)
@@ -55,6 +59,10 @@ class ControladorGeneradorNumerosPseudoaleatorios:
 
     def generar_histograma(self, numeros_pseudoaleatorios, intervalos):
 
+        # Defino mínimo y máximo
+        minimo = 0
+        maximo = 1
+
         # Creo grafico
         fig, ax = plt.subplots()
 
@@ -62,7 +70,7 @@ class ControladorGeneradorNumerosPseudoaleatorios:
         n_bins = cantidad_intervalos
         x = numeros_pseudoaleatorios
 
-        ax.hist(x, n_bins, range=(0, 1), rwidth=0.8, color="navy", label="Frecuencias observadas")
+        ax.hist(x, n_bins, range=(minimo, maximo), rwidth=0.8, color="navy", label="Frecuencias observadas")
         ax.legend(prop={"size": 8})
         ax.set_title("Histograma")
 
@@ -88,26 +96,30 @@ class ControladorGeneradorNumerosPseudoaleatorios:
         # el procesamiento con un algoritmo de ordenamiento de O(n * log n)
         numeros_pseudoaleatorios = sorting.merge(numeros_pseudoaleatorios)
 
-        # Calculo frecuencias por intervalos
+        # Calculo frecuencias observadas por intervalo
         cantidad_intervalos = len(intervalos)
-        frecuencias_x_intervalo = [0] * cantidad_intervalos
+        frecuencias_observadas_x_intervalo = [0] * cantidad_intervalos
         index = 0
         for numero_pseudoaleatorio in numeros_pseudoaleatorios:
             if not (intervalos[index][0] <= numero_pseudoaleatorio < intervalos[index][1]):
                 index += 1
-            frecuencias_x_intervalo[index] += 1
+            frecuencias_observadas_x_intervalo[index] += 1
+
+        # Calculo frecuencias esperadas por intervalo
+        fe = Decimal(len(numeros_pseudoaleatorios) / cantidad_intervalos)
+        frecuencias_esperadas_x_intervalo = [fe] * cantidad_intervalos
 
         # Genero una lista de diccionarios con el calculo de la prueba de chi cuadrado por intervalo y guardo el final
         # en una variable
         chi_cuadrado_x_intervalo = []
-        fe = len(numeros_pseudoaleatorios) / cantidad_intervalos
         fo_acum = 0
         fe_acum = 0
         c_acum = 0
         for i in range(0, cantidad_intervalos):
             intervalo = (intervalos[i][0].quantize(TWOPLACES), intervalos[i][1].quantize(TWOPLACES))
-            fo = frecuencias_x_intervalo[i]
+            fo = frecuencias_observadas_x_intervalo[i]
             fo_acum += fo
+            fe = frecuencias_esperadas_x_intervalo[i]
             fe_acum += fe
             c = ((fo - fe) ** 2) / fe
             c_acum += c
@@ -115,9 +127,9 @@ class ControladorGeneradorNumerosPseudoaleatorios:
                 "intervalo": intervalo,
                 "fo": fo,
                 "fo_acum": round(fo_acum, 4),
-                "fe": round(fe, 4),
+                "fe": fe.quantize(FOURPLACES),
                 "fe_acum": round(fe_acum, 4),
-                "c": round(c, 4),
+                "c": c.quantize(FOURPLACES),
                 "c_acum": round(c_acum, 4)
             })
         chi_cuadrado = round(c_acum, 4)
