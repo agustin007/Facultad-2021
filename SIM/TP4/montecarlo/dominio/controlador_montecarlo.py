e950aab96c2267486e537a6539fd70b2eaf6c3ce
MMariatti <marcosmariatti@hotmail.com>
2021-05-04 20:35:10 -0300

    inicio logica

--- SIM/TP4/montecarlo/dominio/controlador_montecarlo.py
+++ SIM/TP4/montecarlo/dominio/controlador_montecarlo.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-
+import random
 
 class ControladorMontecarlo:
 
@@ -17,7 +17,86 @@ class ControladorMontecarlo:
     stock_minimo = None
 
     def simular_semana(self, vector_estado):
-        pass
+
+        #variables que reflejan el stock
+        
+        stock = 7
+        stock_reabastecimiento = 6
+        stock_minimo = 2
+
+        #variables asociadas a la demanda y al tiempo de espera por pedido
+
+        demanda = 0
+        tiempo_entrega = 0
+
+        #variables asociadas a los costos de inventario
+
+        costo_agotamiento = 3
+        costo_tenencia = 5
+        costo_pedido = 20
+        costo_total = 0
+
+
+        for i in  range(0,cantidad_semanas):
+
+            demanda_random = random.random()
+            entrega_random = random.random()
+            daniado_random = random.random()
+
+            if demanda_random < 0.5:
+
+                demanda = 0
+                stock = stock - demanda
+
+            elif demanda_random >= 0.5 and demanda_random < 0.65:
+
+                demanda = 1
+                stock = stock - demanda
+
+            elif demanda_random >= 0.65 and demanda_random < 0.8:
+
+                demanda = 2
+                stock = stock - demanda
+
+
+            elif demanda_random < 0.8:
+
+                demanda = 3
+                stock = stock - demanda
+
+
+            if stock == stock_minimo or stock < stock_minimo :
+
+                if entrega_random < 0.3 :
+
+                    tiempo_entrega = 1
+
+                elif entrega_random >= 0.3 and entrega_random < 0.7:
+
+                    tiempo_entrega = 2
+
+                elif entrega_random > 0.7:
+
+                    tiempo_entrega = 3
+
+            if stock < demanda :
+
+                costo_total = costo_agotamiento * (demanda - stock)
+
+            elif stock > demanda:
+
+                costo_total = (costo_tenencia * demanda) + (costo_tenencia * (stock - demanda))
+                
+                stock = stock - demanda
+
+
+
+
+
+
+
+
+
 
     def simular_semanas(self, cantidad_semanas, semana_desde, semana_hasta, probabilidad_0_demanda,
                         probabilidad_1_demanda, probabilidad_2_demanda, probabilidad_3_demanda,
