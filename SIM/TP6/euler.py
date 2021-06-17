e6863adbfd2d89fda1000a5ae60b629764daf8ae
MMariatti <marcosmariatti@hotmail.com>
2021-06-17 20:32:58 -0300

    mejoro euler

--- SIM/TP6/euler.py
+++ SIM/TP6/euler.py
@@ -2,32 +2,55 @@ def funcion(C,T):
 	return C+0.2*T
 
 
-def euler(T0,C0,h,Iteraciones):
+def euler(T,C,h,D,step):
+	t=0
+	D0=0
+	ti=0
+	dD_dt=0
 
-	t=0  #Tiempo inicial
-	D0=0 #Condicion Inicial
-	print('\n-----------SOLUCION-----------')
-	print('------------------------------')    
-	print('t\tD0\tPendiente\tDn')
-	print('------------------------------')
-	for i in range(Iteraciones):
-		pendiente = funcion(C0,T0)
-		Dn = D0+pendiente*h
-		print('%.4f\t%.4f\t%0.4f\t%.4f'% (t,D0,pendiente,Dn))
-		print('------------------------------')
-		D0=Dn
-		t+=h
+	for i in range(0,step):
+		if i == 0:
+			
+			Dn=funcion(C,T)
+			dD_dt= Dn*h
+			
+			print('\n-----------SOLUCION-----------')
+			print('-------------------------------------------------')    
+			print('t\tD0\tdD_dt\tDn\tti')
+			print('-------------------------------------------------')
+			print('%.4f\t%.4f\t%0.4f\t%.4f\t%.4f'% (t,D0,dD_dt,Dn,ti))
+			print('-------------------------------------------------')
+			D0=Dn
+			t+=h
+			ti+=t+h
+
+		else:
+			dD_dt=h*D0
+			Dn = D0+h*D0
+			print('%.4f\t%.4f\t%0.4f\t%.4f\t%.4f'% (t,D0,dD_dt,Dn,ti))
+			print('-------------------------------------------------')
+			D0=Dn
+			t+=h
+			ti+=h
+		if D0>=D:
+			print("En : ",round(t,3),", D :",D)
+			break
+
+
 
 print("Calculo por metodo de Euler")
 cInicial= float(input("Ingrese la cantidad de autos que hay en la cola :"))
 print('------------------------------')
 cteT=float(input("Ingrese el valor de la constante T :"))
 print('------------------------------')
+auto=int(input("Auto grande(180) o el resto de los autos(130) :"))
+print('------------------------------')
 step=float(input("Ingrese el valor de h : "))
+print('------------------------------')
 pasos=int(input("Ingrese la cantidad de Iteraciones : "))
 print('------------------------------')
 
-euler(cteT,cInicial,step,pasos)
+euler(cteT,cInicial,step,auto,pasos)
 
 
 
